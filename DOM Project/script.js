6d367186c49ad9ef313222b3ca2c04181e4156a4
JIGARHUSSAIN <jigarhussain793@gmail.com>
2024-10-18 17:46:52 +0530

    Add files via upload

---
+++ DOM Project/script.js
@@ -0,0 +1,27 @@
+let istatus = document.querySelector("h5")
+
+let btn= document.querySelector("#add")
+let check = 0
+
+
+btn.addEventListener("click",()=>{
+    if(check == 0){
+        
+        istatus.innerHTML = "Friends"
+        istatus.style.color = "green"
+        btn.innerHTML = "Remove"
+        btn.style.backgroundColor = "grey"
+        check = 1
+    }
+    else{
+        istatus.innerHTML = "Stranger"
+        istatus.style.color = "red"
+        btn.innerHTML = "Add Friend"
+        btn.style.backgroundColor = "cadetblue"
+        check = 0
+
+        
+
+    }
+})
+
